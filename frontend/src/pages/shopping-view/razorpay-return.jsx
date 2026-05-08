12fab8d2624457144eb7b0db08fcf6b7302d0dbc
MOHD NAWAZ KHAN <khanmohdnawaz567@gmail.com>
2026-05-09 01:18:53 +0530

    fix order success page

--- frontend/src/pages/shopping-view/razorpay-return.jsx
+++ frontend/src/pages/shopping-view/razorpay-return.jsx
@@ -2,14 +2,15 @@ import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { capturePayment } from "@/store/shop/order-slice";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function RazorpayReturnPage() {
   const dispatch = useDispatch();
   const location = useLocation();
+  const navigate = useNavigate();
+
   const params = new URLSearchParams(location.search);
 
-  // Razorpay params
   const razorpay_payment_id = params.get("razorpay_payment_id");
   const razorpay_order_id = params.get("razorpay_order_id");
   const razorpay_signature = params.get("razorpay_signature");
@@ -28,11 +29,18 @@ function RazorpayReturnPage() {
       ).then((data) => {
         if (data?.payload?.success) {
           sessionStorage.removeItem("currentOrderId");
-          window.location.href = "/shop/payment-success";
+
+          navigate("/shop/payment-success");
         }
       });
     }
-  }, [razorpay_payment_id, razorpay_order_id, razorpay_signature, dispatch]);
+  }, [
+    razorpay_payment_id,
+    razorpay_order_id,
+    razorpay_signature,
+    dispatch,
+    navigate,
+  ]);
 
   return (
     <Card>
