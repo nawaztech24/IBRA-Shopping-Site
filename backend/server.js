d3878caae1d2cab50d0e8654640731bea8d351ec
MOHD NAWAZ KHAN <khanmohdnawaz567@gmail.com>
2026-05-08 21:30:13 +0530

    fixed cors and api urls

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); 
+require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -18,17 +18,14 @@ const shopReviewRouter = require("./routes/shop/review-routes");
 
 const commonFeatureRouter = require("./routes/common/feature-routes");
 
-
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log(" MongoDB connected"))
-  .catch((error) => console.log(" MongoDB error:", error));
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log("MongoDB error:", error));
 
 const app = express();
 
-
 const PORT = process.env.PORT || 5000;
-
 
 app.use(
   cors({
@@ -36,21 +33,12 @@ app.use(
       "http://localhost:5173",
       "https://shopping-app-one-zeta.vercel.app",
     ],
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
     credentials: true,
   })
 );
 
 app.use(cookieParser());
 app.use(express.json());
-
 
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
@@ -68,7 +56,6 @@ app.use("/api/common/feature", commonFeatureRouter);
 app.get("/", (req, res) => {
   res.send("Backend server is running successfully🚀");
 });
-
 
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
