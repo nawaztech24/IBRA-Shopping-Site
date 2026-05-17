74e09bb7a369bcbb5e502bec0e8e7fab4d7be0b4
MOHD NAWAZ KHAN <khanmohdnawaz567@gmail.com>
2026-05-18 02:19:20 +0530

    fixed header

--- frontend/src/components/shopping-view/header.jsx
+++ frontend/src/components/shopping-view/header.jsx
@@ -162,7 +162,7 @@ function ShoppingHeader() {
   const { isAuthenticated } = useSelector((state) => state.auth);
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
+    <header className="fixed top-0 z-40 w-full border-b bg-background">
 
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
 
--- frontend/src/components/shopping-view/layout.jsx
+++ frontend/src/components/shopping-view/layout.jsx
@@ -6,7 +6,7 @@ function ShoppingLayout() {
     <div className="flex flex-col bg-white overflow-hidden">
       {/* common header */}
       <ShoppingHeader />
-      <main className="flex flex-col w-full">
+      <main className="flex flex-col w-full pt-16">
         <Outlet />
       </main>
     </div>
